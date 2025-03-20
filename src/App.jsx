5556c51ba5ea15b36397f5ed37a80a3218bc8ca2
yeswanth274 <samuelpola796@gmail.com>
2025-03-20 14:18:11 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+function App() {
+  const books = [
+    {
+      id: 1,
+      image:
+        'https://tse2.mm.bing.net/th?id=OIP.IIIPIPZYMBOJgxFfWGRtBAHaHa&rs=1&pid=ImgDetMain',
+      name: 'The Great Adventure',
+      genre: 'Fiction',
+      author: 'Jane Doe',
+    },
+    {
+      id: 2,
+      image: 'https://i.thenile.io/r1000/9781496687197.jpg?r=6085750d133cf',
+      name: 'Mysteries of the Universe',
+      genre: 'Science',
+      author: 'John Smith',
+    },
+    {
+      id: 3,
+      image: 'https://images3.penguinrandomhouse.com/cover/9781933771106',
+      name: 'History Revisited',
+      genre: 'Non-Fiction',
+      author: 'Emily Johnson',
+    },
+    {
+      id: 4,
+      image:
+        'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1700317474i/202368563.jpg',
+      name: 'Culinary Delights',
+      genre: 'Cooking',
+      author: 'Michael Brown',
+    },
+  ];
+
+  return <>{/* write code to display all the books using */}</>;
+}
+
+export default App;
